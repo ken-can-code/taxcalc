ba66594b7fc09ed90215446c0254d9134861acba
Ken S <gxcadr@gmail.com>
2021-09-11 00:25:39 -0700

    selected all items

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 let stateRules;
+const states = document.getElementById('states');
+const maritalStatus = document.getElementsByName('maritalStatus');
+const mooch = document.getElementById('mooch');
+const grossIncome = document.getElementById('grossIncome');
+const submitButton = document.getElementById('formSubmit');
 
-if (state === texas) {
-  stateRules = texasRules;
-} else if (state === califorsia) {
-  blah blah
+console.log('maritalStatus', maritalStatus);
+console.log('mooch', mooch);
+
+// if (state === texas) {
+//   stateRules = texasRules;
+// } else if (state === 'California') {
+//   blah blah
+// }
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log('maritalStatus inside handleSubmit', maritalStatus);
+  console.log('event', event.target);
 }
 
-*grabbed income input*  * stateRules.multiplier
 
-{
-  
+submitButton.addEventListener('click', handleSubmit);
+
+// *grabbed income input*  * stateRules.multiplier
+let userSelectedMaritalStatus;
+
+for (let i = 0; i < maritalStatus.length; i += 1) {
+  if (maritalStatus[i].checked === true) {
+    userSelectedMaritalStatus = maritalStatus[i].value;
+  }
 }
