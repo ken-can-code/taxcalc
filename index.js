7e5e2e914d9718a290f1775fbebc4da98c12cd94
Ken S <gxcadr@gmail.com>
2021-09-14 23:43:46 -0700

    worked on collecting mooch data

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let stateRules;
 const states = document.getElementById('states');
 const maritalStatus = document.getElementsByName('maritalStatus');
-const mooch = document.getElementById('mooch');
 const grossIncome = document.getElementById('grossIncome');
 const submitButton = document.getElementById('formSubmit');
 
@@ -9,25 +8,29 @@ console.log('maritalStatus', maritalStatus);
 console.log('mooch', mooch);
 
 // if (state === texas) {
-//   stateRules = texasRules;
-// } else if (state === 'California') {
-//   blah blah
-// }
-
+  //   stateRules = texasRules;
+  // } else if (state === 'California') {
+    //   blah blah
+let userSelectedMaritalStatus;
+let moochVal;
+let mooch = document.getElementById('mooch');
 function handleSubmit(event) {
   event.preventDefault();
-  console.log('maritalStatus inside handleSubmit', maritalStatus);
-  console.log('event', event.target);
+  // console.log('maritalStatus inside handleSubmit', maritalStatus);
+  // console.log('event', event.target);
+  for (let i = 0; i < maritalStatus.length; i += 1) {
+    if (maritalStatus[i].checked === true) {
+      userSelectedMaritalStatus = maritalStatus[i].value;
+      break;
+    }
+  }
+  console.log('userSelectedMaritalStatus', userSelectedMaritalStatus);
+  mooch = mooch.value;
+  // let moochNum = mooch;
+  console.log('moochNum', mooch);
 }
 
 
 submitButton.addEventListener('click', handleSubmit);
 
 // *grabbed income input*  * stateRules.multiplier
-let userSelectedMaritalStatus;
-
-for (let i = 0; i < maritalStatus.length; i += 1) {
-  if (maritalStatus[i].checked === true) {
-    userSelectedMaritalStatus = maritalStatus[i].value;
-  }
-}
