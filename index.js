f9df7a2b7bcbb8955f33a1bcc1375c29f43ba291
Ken S <gxcadr@gmail.com>
2021-09-16 00:07:24 -0700

    gathered all inputs

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-let stateRules;
-const states = document.getElementById('states');
+let stateRules = { // one option for dynamically calculating tax
+  Califoria: '1.2x',
+  Texas: '2.5x',
+}
+const results = document.getElementById('results');
 const maritalStatus = document.getElementsByName('maritalStatus');
-const grossIncome = document.getElementById('grossIncome');
 const submitButton = document.getElementById('formSubmit');
 
-console.log('maritalStatus', maritalStatus);
-console.log('mooch', mooch);
-
-// if (state === texas) {
-  //   stateRules = texasRules;
-  // } else if (state === 'California') {
-    //   blah blah
 let userSelectedMaritalStatus;
-let moochVal;
-let mooch = document.getElementById('mooch');
+let mooch;
+let grossIncome;
+let state;
 function handleSubmit(event) {
   event.preventDefault();
-  // console.log('maritalStatus inside handleSubmit', maritalStatus);
-  // console.log('event', event.target);
   for (let i = 0; i < maritalStatus.length; i += 1) {
     if (maritalStatus[i].checked === true) {
       userSelectedMaritalStatus = maritalStatus[i].value;
       break;
     }
   }
-  console.log('userSelectedMaritalStatus', userSelectedMaritalStatus);
-  mooch = mooch.value;
-  // let moochNum = mooch;
-  console.log('moochNum', mooch);
+  // console.log('userSelectedMaritalStatus', userSelectedMaritalStatus);
+  mooch = document.getElementById('mooch').value;
+  // console.log('mooch', mooch);
+  grossIncome = document.getElementById('grossIncome').value;
+  // console.log ('gross!', grossIncome);
+  state = document.getElementById('states').value;
+  // console.log ('state', state);
+
+  resultsDisplay();
 }
 
-
+function resultsDisplay() {
+  results.innerHTML = (
+  `<p>State: ${state}</p>
+   <p>Marital Status: ${userSelectedMaritalStatus}</p>
+   <p>Dependents: ${mooch}</p>
+   <p>Gross Income: ${grossIncome}</p><br>
+   
+   <p>Based on the info you've given us, your state income taxes will be: $50000</p>
+  `)
+}
 submitButton.addEventListener('click', handleSubmit);
 
 // *grabbed income input*  * stateRules.multiplier
