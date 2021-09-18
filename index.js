1ae872218e7290fb64ab56cc3648ef4dc9582b8e
Ken S <gxcadr@gmail.com>
2021-09-18 00:24:08 -0700

    tax brackets pseudocode added

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-let stateRules = { // one option for dynamically calculating tax
-  Califoria: '1.2x',
-  Texas: '2.5x',
-}
+let stateRules = {
+  California: {
+    '10000': .05,
+    '30000': .07,
+    SanFrancisco: 'placeholder',
+    SanMateo: 'placeholderier',
+  },
+  
+  Texas: {
+    SanAntonio: 'is it a county?',
+  },
+};
+
 const results = document.getElementById('results');
 const maritalStatus = document.getElementsByName('maritalStatus');
 const submitButton = document.getElementById('formSubmit');
 
 let userSelectedMaritalStatus;
-let mooch;
+let numberOfDependents;
 let grossIncome;
 let state;
 function handleSubmit(event) {
@@ -19,13 +28,30 @@ function handleSubmit(event) {
     }
   }
   // console.log('userSelectedMaritalStatus', userSelectedMaritalStatus);
-  mooch = document.getElementById('mooch').value;
-  // console.log('mooch', mooch);
+  numberOfDependents = document.getElementById('dependents').value;
+  // console.log('dependents', numberOfDependents);
   grossIncome = document.getElementById('grossIncome').value;
   // console.log ('gross!', grossIncome);
   state = document.getElementById('states').value;
   // console.log ('state', state);
-
+let bracket;
+  // ${enteredIncome} * stateRules.${enteredState}.${bracket}
+  if (grossIncome > stateRules.state.bracket1) {
+    if (grossIncome > stateRules.state.bracket2) {
+      if (grossIncome > stateRules.state.bracket3) {
+        bracket = 'bracket4';
+      } else {
+        bracket = 'bracket3';
+      }
+    } else {
+      bracket = 'bracket2';
+    }
+  } else {
+    bracket = 'bracket1';
+  }
+  const displayAmount = grossIncome * stateRules.state.bracket
+  const displaySection = document.getElementById('displayTaxAmount');
+  displaySection.textContent = displayAmount;
   resultsDisplay();
 }
 
