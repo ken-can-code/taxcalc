3bcb2c596f0c24c06163a5046ca513fd79942b0c
Ken S <gxcadr@gmail.com>
2021-09-23 00:26:06 -0700

    function for brackets concept finished

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ let stateRules = {
   },
 };
 
+function taxCalculation(grossIncome) {
+  // We'll assume grossIncome is $20000
+  
+  let taxSum = 0;
+  
+  function calculateTaxForBracket(ceilingOfBracket, floorOfBracket, taxRate) {
+    const maxAmount = ceilingOfBracket - floorOfBracket;
+    taxSum += (grossIncome > ceilingOfBracket ? maxAmount : grossIncome - floorOfBracket) * taxRate;
+  }
+
+  if (grossIncome >= 0) {
+    calculateTaxForBracket(8808.99, 0, .01);
+  }
+
+  if (grossIncome >= 8809) {
+    calculateTaxForBracket(20882.99, 8809, .02);
+  }
+
+  if (grossIncome >= 20883) {
+    calculateTaxForBracket(32960, 20883, .04);
+  }
+
+  return taxSum.toFixed(2);
+}
+
 const results = document.getElementById('results');
 const maritalStatus = document.getElementsByName('maritalStatus');
 const submitButton = document.getElementById('formSubmit');
@@ -27,31 +52,29 @@ function handleSubmit(event) {
       break;
     }
   }
-  // console.log('userSelectedMaritalStatus', userSelectedMaritalStatus);
+
   numberOfDependents = document.getElementById('dependents').value;
-  // console.log('dependents', numberOfDependents);
   grossIncome = document.getElementById('grossIncome').value;
-  // console.log ('gross!', grossIncome);
   state = document.getElementById('states').value;
-  // console.log ('state', state);
-let bracket;
-  // ${enteredIncome} * stateRules.${enteredState}.${bracket}
-  if (grossIncome > stateRules.state.bracket1) {
-    if (grossIncome > stateRules.state.bracket2) {
-      if (grossIncome > stateRules.state.bracket3) {
-        bracket = 'bracket4';
-      } else {
-        bracket = 'bracket3';
-      }
-    } else {
-      bracket = 'bracket2';
-    }
-  } else {
-    bracket = 'bracket1';
-  }
-  const displayAmount = grossIncome * stateRules.state.bracket
-  const displaySection = document.getElementById('displayTaxAmount');
-  displaySection.textContent = displayAmount;
+  console.log('grossIncome', grossIncome);
+// let bracket;
+//   // ${enteredIncome} * stateRules.${enteredState}.${bracket}
+//   if (grossIncome > stateRules.state.bracket1) {
+//     if (grossIncome > stateRules.state.bracket2) {
+//       if (grossIncome > stateRules.state.bracket3) {
+//         bracket = 'bracket4';
+//       } else {
+//         bracket = 'bracket3';
+//       }
+//     } else {
+//       bracket = 'bracket2';
+//     }
+//   } else {
+//     bracket = 'bracket1';
+//   }
+  // const displayAmount = grossIncome * stateRules.state.bracket
+  // const displaySection = document.getElementById('displayTaxAmount');
+  // displaySection.textContent = displayAmount;
   resultsDisplay();
 }
 
@@ -59,8 +82,8 @@ function resultsDisplay() {
   results.innerHTML = (
   `<p>State: ${state}</p>
    <p>Marital Status: ${userSelectedMaritalStatus}</p>
-   <p>Dependents: ${mooch}</p>
-   <p>Gross Income: ${grossIncome}</p><br>
+   <p>Dependents: ${numberOfDependents}</p>
+   <p>Gross Income: ${taxCalculation(grossIncome)}</p><br>
    
    <p>Based on the info you've given us, your state income taxes will be: $50000</p>
   `)
