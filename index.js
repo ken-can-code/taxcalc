e5dc7e0f7db594ee3a6b11ac7ead05779b601870
Ken S <gxcadr@gmail.com>
2021-09-24 23:37:32 -0700

    import statetaxinfo from external file

--- index.js
+++ index.js
@@ -1,58 +1,3 @@
-let stateRules = {
-  California: {
-    Married: {
-      taxBrackets: [0,
-        17618,
-        41766,
-        65920,
-        91506,
-        115648,
-        590746,
-        708890,
-        1000000,
-        1181484,
-        Infinity],
-      taxRates: [.01,
-        .02,
-        .04,
-        .06,
-        .08,
-        .093,
-        .103,
-        .113,
-        .123,
-        .133],
-    },
-    Single: {
-      taxBrackets: [0,
-        8809,
-        20883,
-        32960,
-        45753,
-        57824,
-        295373,
-        354445,
-        590742,
-        1000000,
-        Infinity],
-      taxRates: [.01,
-        .02,
-        .04,
-        .06,
-        .08,
-        .093,
-        .103,
-        .113,
-        .123,
-        .133],
-    },
-  },
-  
-  Texas: {
-    SanAntonio: 'is it a county?',
-  },
-};
-
 function taxCalculation(grossIncome) {
   let taxSum = 0;
   
@@ -60,7 +5,7 @@ function taxCalculation(grossIncome) {
     const maxAmount = ceilingOfBracket - floorOfBracket;
     taxSum += (grossIncome > ceilingOfBracket ? maxAmount : grossIncome - floorOfBracket) * taxRate;
   }
-  const stateAndMaritalStatus = stateRules[state][userSelectedMaritalStatus];
+  const stateAndMaritalStatus = stateTaxInfo[state][userSelectedMaritalStatus];
   for (let i = 0; grossIncome > stateAndMaritalStatus.taxBrackets[i]; i += 1) {
     calculateTaxForBracket(stateAndMaritalStatus.taxBrackets[i + 1],
       stateAndMaritalStatus.taxBrackets[i],
