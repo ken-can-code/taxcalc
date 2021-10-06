7ca443bd692bdde16a839d0872cd05b9146b62e4
Ken S <gxcadr@gmail.com>
2021-10-06 01:14:08 -0700

    some work with asyncronous code

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-function taxCalculation(grossIncome) {
-  let taxSum = 0;
-  
-  function calculateTaxForBracket(ceilingOfBracket, floorOfBracket, taxRate) {
-    const maxAmount = ceilingOfBracket - floorOfBracket;
-    taxSum += (grossIncome > ceilingOfBracket ? maxAmount : grossIncome - floorOfBracket) * taxRate;
-  }
-
-  async function getStateTax(url) {
-    const stateTaxInfoObj = await fetch(url);
-    const parsedData = await stateTaxInfoObj.json();
-    return parsedData;
-  }
-
-  const stateTaxInfo = getStateTax(`https://data.ftb.ca.gov/resource/hqma-83bw.json?taxable_year=${taxYear}&filing_status=${userSelectedMaritalStatus}`);
-  console.log('stateTaxInfo', stateTaxInfo);
-  // const stateAndMaritalStatus = stateTaxInfo[state][userSelectedMaritalStatus];
-  // for (let i = 0; grossIncome > stateAndMaritalStatus.taxBrackets[i]; i += 1) {
-  //   calculateTaxForBracket(stateAndMaritalStatus.taxBrackets[i + 1],
-  //     stateAndMaritalStatus.taxBrackets[i],
-  //     stateAndMaritalStatus.taxRates[i]);
-  //     // console.log('SAMS', stateAndMaritalStatus.taxBrackets[i + 1], stateAndMaritalStatus.taxBrackets[i]);
-  //     // console.log('taxRates', stateAndMaritalStatus.taxRates[i]);
-  // }
-
-  // return taxSum.toFixed(2);
-  // return stateTaxInfo;
-  return 1234.56;
-}
-
 const results = document.getElementById('results');
 const maritalStatus = document.getElementsByName('maritalStatus');
 const submitButton = document.getElementById('formSubmit');
@@ -56,9 +26,58 @@ function handleSubmit(event) {
   resultsDisplay();
 }
 
+async function getStateTax(url) {
+  const stateTaxInfoObj = await fetch(url);
+  const parsedData = await stateTaxInfoObj.json();
+  console.log('parsed data', parsedData);
+  return parsedData;
+}
+
+async function taxCalculation(grossIncome) {
+  let taxSum = 0;
+  
+  function calculateTaxForBracket(ceilingOfBracket, floorOfBracket, taxRate) {
+    const maxAmount = ceilingOfBracket - floorOfBracket;
+    taxSum += (grossIncome > ceilingOfBracket ? maxAmount : grossIncome - floorOfBracket) * taxRate;
+  }
+
+  const stateTaxInfo = await getStateTax(`https://data.ftb.ca.gov/resource/hqma-83bw.json?taxable_year=${taxYear}&filing_status=${userSelectedMaritalStatus}`);
+  console.log('stateTaxInfo', stateTaxInfo); // an array of objects
+  // "taxable_income" -> parse out ending number
+  // "tax_rate_percentage" -> use this number to calculate tax
+
+  function parseTaxBracket(taxBracketStr) {
+    let startIdx;
+    for (let i = taxBracketStr.length - 1; i >= 0; i -= 1) {
+      const char = taxBracketStr[i];
+      if (char === ' ') {
+        startIdx = i + 1;
+      }
+    }
+    let numStr = '';
+    for (let i = startIdx; i < taxBracketStr.length; i += 1) {
+      const char = taxBracketStr[i];
+      if (char !== ',') {
+        numStr += char;
+      }
+    }
+    return +numStr;
+  }
+
+  for (let i = 0; i < stateTaxInfo.length - 1; i += 1) {
+    const eachBracketObj = stateTaxInfo[i];
+    const taxBracketCeiling = parseTaxBracket(eachBracketObj.taxable_income);
+    console.log('taxBracketNumber Working!', typeof taxBracketCeiling, taxBracketCeiling);
+  }
+
+  // return taxSum.toFixed(2);
+  // return stateTaxInfo;
+  // return 1234.56;
+}
+
 function resultsDisplay() {
   const taxDisplay = taxCalculation(grossIncome);
-  console.log(taxDisplay);
+  console.log('tax display', taxDisplay);
   if (userSelectedMaritalStatus === 'Married Filing Jointly') {
     userSelectedMaritalStatus = 'Married';
     console.log('working up to here');
@@ -66,7 +85,7 @@ function resultsDisplay() {
   results.innerHTML = (
   `<p>Based on the info you've given us, your state
   income taxes will be: $${taxDisplay}</p><br>
-   <p>Tax Year: ${year}</p>
+   <p>Tax Year: ${taxYear}</p>
    <p>Gross Income: ${grossIncome}</p>
    <p>State: ${state}</p>
    <p>Marital Status: ${userSelectedMaritalStatus}</p>
